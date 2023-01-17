4b17cbfb5a28172b0f05273256b6555f31ec35e9
Sokrates Bousios <sokratesb@gmail.com>
2023-01-17 19:56:54 +0100

    Refactoring - naming and params of functions in creator_my_sql.

--- sql_db_creator/src/creator_mysql.rs
+++ sql_db_creator/src/creator_mysql.rs
@@ -23,15 +23,14 @@ pub struct Config {
     pub host: String
 }
 
-fn db_url(config: &Config, db_name: Option<&str>) -> String {
+async fn create_connection(config: &Config) -> Result<Pool<MySql>, Error> {
     let user = &config.user;
     let password = &config.password;
     let host = &config.host;
 
-    match db_name {
-        Some(db_name) => return format!("mysql://{user}:{password}@{host}/{db_name}"),
-        None => return format!("mysql://{user}:{password}@{host}")
-    }
+    let url = format!("mysql://{user}:{password}@{host}");
+
+    MySqlPool::connect(&url).await
 }
 
 async fn create_db(name: &str, pool: &Pool<MySql>) -> Result<MySqlQueryResult, Error> {
@@ -39,44 +38,61 @@ async fn create_db(name: &str, pool: &Pool<MySql>) -> Result<MySqlQueryResult, E
     sqlx::query(&query).execute(pool).await
 }
 
-async fn create_pool(config: &Config, db_name: Option<&str>) -> Result<Pool<MySql>, Error> {
-    let url = db_url(config, db_name);
+async fn create_pool_for_db(config: &Config, db_name: &str) -> Result<Pool<MySql>, Error> {
+    let user = &config.user;
+    let password = &config.password;
+    let host = &config.host;
+
+    let url = format!("mysql://{user}:{password}@{host}/{db_name}");
 
     MySqlPool::connect(&url).await
 }
 
-fn create_pools(config: &Config, dbs: &DBs, pool: &Pool<MySql>) -> Vec::<Pool<MySql>> {
+fn create_dbs(config: &Config, dbs: &DBs, pool: &Pool<MySql>) -> Vec::<Pool<MySql>> {
     let mut pools: Vec::<Pool<MySql>>  = Vec::new();
 
     let dbs = &dbs.dbs;
     for db in dbs {
         match block_on(create_db(&db.name, &pool)) {
             Ok(_) => {
-                println!("Database created: {}", &db.name);
-
-                match block_on(create_pool(config, Some(&db.name))) {
-                    Ok(pool) => pools.push(pool),
-                    Err(e) => println!("Something went wron by trying to create a pool: {e}")
+                println!("New Database with name \"{}\" created", &db.name);
+                match block_on(create_pool_for_db(config, &db.name)) {
+                    Ok(pool) => {
+                        pools.push(pool);
+                    },
+                    Err(e) => {
+                        println!("Error: {e}");
+                        println!("Something went wron by trying to create the pool for the \"{}\" database", &db.name);
+                    }
                 }
             },
-            Err(e) => println!("Database could't be created: {e}")
+            Err(e) => {
+                println!("Error: {e}");
+                println!("Database with name \"{}\" couldn't be created", &db.name);
+            }
         }
     }
 
     pools
 }
 
+// fn create_table(pool: &Pool<MySql>) {
+
+// }
+
 pub fn setup(config: Config) {
     let dbs = DBs::new();
 
     //dbs.print_db();
 
-    let pool_future_result = create_pool(&config, None);
+    let pool_future_result = create_connection(&config);
 
     match block_on(pool_future_result) {
         Ok(pool) => {
-            let pools = create_pools(&config, &dbs, &pool);
+            let pools = create_dbs(&config, &dbs, &pool);
+
+            //println!("Connection to Database established");
         },
-        Err(e) => println!("Pool could't be created: {e}")
+        Err(e) => println!("One or mor pools couldn't be created")
     }
 }
