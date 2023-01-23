4723fde849dc9b43bec0e4fa4249fa519b8b2066
Sokrates Bousios <sokratesb@gmail.com>
2023-01-23 15:39:30 +0100

    crate included.

--- sql_db_creator_mysql_example/src/main.rs
+++ sql_db_creator_mysql_example/src/main.rs
@@ -1,16 +1,11 @@
-mod secure;
-use secure::Credentials;
-
 use sql_db_creator::{ DBType, Config, setup };
 
 fn main() {
 
-    let credentials = Credentials::new();
-
     let config = Config {
-        user: credentials.user,
-        password: credentials.password,
-        host: credentials.host
+        user: String::from("root"),
+        password: String::from("password"),
+        host: String::from("localhost")
     };
 
     setup(DBType::MySql, config);
